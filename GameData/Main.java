b1a3c8be7bfc46bde416d7ed4cdd3dfc7ad1d4d5
Cool-Guy1999 <0jaseel0@gmail.com>
2015-07-07 13:46:33 +0530

    Update Main.java

--- GameData/Main.java
+++ GameData/Main.java
@@ -2,10 +2,13 @@ public class Main {
 
 	public static final int NUMBER_OF_GAMES = 10; //Number of games to play
 	public static final long MAX_TURN_TIME = 200*1000000; // Nanoseconds per Turn (200ms)
-	public static final int sleepTime = 100; //Time to wait between turns
+	public static final int sleepTime = 750; //Time to wait between turns (in ms)
 	
+	private int robberTotal = 0;
+	private int copTotal    = 0; 
 	private int robberMoves = 0;
 	private int copMoves    = 0; 
+	private int gameNo      = 1;
 
 	Main(Cop c, Robber r) {
 		System.out.println("Starting game " + c.getName() + " vs " + r.getName());
@@ -13,60 +16,72 @@ public class Main {
 	}
 	
 	private void playGame(Cop c, Robber r) {
-		Grid grid = new Grid();
-		
-		boolean copWinner = false;
-		
-		while(true) {
-		
-			Grid clone = new Grid(grid);
+	
+		while(gameNo <= NUMBER_PF_GAMES){
+			Grid grid = new Grid();
 			
-			long startTurnTime = System.nanoTime();//Measure time
+			boolean copWinner = false;
 			
-			int move1 = c.takeTurn();
+			while(true) {
 			
-			if(System.nanoTime() - startTurnTime > MAX_TURN_TIME) {
-				System.out.println(c.getName() + " has been disqualified for taking more than 200 ms");
-				System.exit(-1);
-			}
-			
-			if(grid.isValidMove(grid, move1)){
-				copMoves++;
-				if(grid.checkWinner(move1, 0))
-					break;
-			}
-			
-			grid.printGrid();
-			try{Thread.sleep(sleepTime);}catch(InterruptedException e){System.out.println("Sleep INTERRUPTED!");System.exit(1);}
-						
-			startTurnTime = System.nanoTime();//Measure time
-						
-			int move2 = r.takeTurn();
-			
-			if(System.nanoTime() - startTurnTime > MAX_TURN_TIME) {
-				System.out.println(r.getName() + " has been disqualified for taking more than 200 ms");
-				System.exit(-1);
-			}
-			
-			if(grid.isValidPosition(grid, move2)) {
-				robberMoves++;
-				if(grid.checkWinner(move2, 1)) {
-					break;
+				long startTurnTime = System.nanoTime();//Measure time
+				
+				int move1 = c.takeTurn();
+				
+				if(System.nanoTime() - startTurnTime > MAX_TURN_TIME) {
+					System.out.println(c.getName() + " has been disqualified for taking more than 200 ms");
+					System.exit(-1);
 				}
-				grid.updateGrid(move2, 1);
+				
+				if(grid.isValidMove(move1)){
+					copMoves++;
+					if(grid.checkWinner(move1, 0))
+						break;
+					grid.updateGrid(move1, 0);
+				}
+				
+				grid.printGrid();
+				try{Thread.sleep(sleepTime);}catch(InterruptedException e){System.out.println("Sleep INTERRUPTED!");System.exit(1);}
+							
+				startTurnTime = System.nanoTime();//Measure time
+							
+				int move2 = r.takeTurn();
+				
+				if(System.nanoTime() - startTurnTime > MAX_TURN_TIME) {
+					System.out.println(r.getName() + " has been disqualified for taking more than 200 ms");
+					System.exit(-1);
+				}
+				
+				if(grid.isValidPosition(move2)) {
+					robberMoves++;
+					if(grid.checkWinner(move2, 1)) {
+						break;
+					}
+					grid.updateGrid(move2, 1);
+				}
+				
+				grid.printGrid();
+				try{Thread.sleep(sleepTime);}catch(InterruptedException e){System.out.println("Sleep INTERRUPTED!");System.exit(1);}
 			}
 			
 			grid.printGrid();
-			try{Thread.sleep(sleepTime);}catch(InterruptedException e){System.out.println("Sleep INTERRUPTED!");System.exit(1);}
+			
+			printScores();
+			gameNo++;
+			
+			robberTotal += robberMoves;
+			copTotal += copMoves;
+			
+			robberMoves=copMoves=0;
 		}
 		
-		grid.printGrid();
-		System.out.println("Game ended!");
-		
-		printScores();
+		System.out.println("\n\nFINAL SCORES:\n");
+		System.out.println("Cop("+c.getName()+") = " + copTotal);
+		System.out.println("Robber("+r.getName()+") = " + robberTotal);
 	}
 	
 	private void printScores() {
+		System.out.println("Game #" + gameNo + " scores:");
 		System.out.println("Score of the robber = " + robberMoves);
 		System.out.println("Score of the cop = " + copMoves);
 	}
